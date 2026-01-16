428b62901c90af2629670fefa64c6415e7928c93
Arnold M S <epten08@gmail.com>
2026-01-16 13:51:17 +0200

    mock dimensions,naimated,flatlist

--- __mocks__/react-native.js
+++ __mocks__/react-native.js
@@ -3,6 +3,9 @@ module.exports = {
   Text: 'Text',
   TouchableOpacity: 'TouchableOpacity',
   ScrollView: 'ScrollView',
+  FlatList: 'FlatList',
+  TextInput: 'TextInput',
+  KeyboardAvoidingView: 'KeyboardAvoidingView',
   Alert: {
     alert: jest.fn(),
   },
@@ -13,5 +16,15 @@ module.exports = {
   Platform: {
     OS: 'ios',
     select: obj => obj.ios,
+  },
+  Dimensions: {
+    get: () => ({ width: 375, height: 812 }),
+  },
+  Animated: {
+    View: 'Animated.View',
+    Value: jest.fn(() => ({
+      interpolate: jest.fn(() => 0),
+    })),
+    event: jest.fn(() => jest.fn()),
   },
 };
